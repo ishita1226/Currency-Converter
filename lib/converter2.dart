0dc805c1085c5bd00ca4b35a854aa93c2ee0770e
ishita1226 <ishitashukla120603@gmail.com>
2024-03-10 19:37:14 +0530

    another day

--- lib/converter2.dart
+++ lib/converter2.dart
@@ -7,16 +7,50 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return const Scaffold(
-        body: ColoredBox(
-          color: Colors.cyan,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Heyyyyy !!!'),
-            ],
-          ),
+        backgroundColor : Colors.blueGrey,
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Currency Converter ',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w100), ),
+            Padding(
+              padding: EdgeInsets.all(11.0),
+              child: TextField(style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                      hintText: "Enter amount in USD",
+                    hintStyle: TextStyle(color: Colors.black38),
+                    prefixIcon: Icon(Icons.monetization_on_outlined),
+                    prefixIconColor: Colors.black54,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder:  OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                         style: BorderStyle.solid,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5))
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2,
+                          style: BorderStyle.solid,
+                          strokeAlign: BorderSide.strokeAlignOutside,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(5))
+                    ),
+
+              ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+
+              ),
+            )
+          ],
         )
+
     );
   }
 }
